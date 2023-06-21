4d1574ad29841232f153c8ca9d51c93c1c943ec6
Diego Resendiz Ojeda <di_564@hotmail.com>
2023-06-21 03:35:39 -0700

    migraciones

--- VillaApi/Datos/ApplicationDbContext.cs
+++ VillaApi/Datos/ApplicationDbContext.cs
@@ -12,5 +12,38 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Villa> Villas { get; set; }
     public DbSet<NumeroVilla> NumeroVillas  { get; set; }
-   
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Villa>().HasData(
+            new Villa()
+            {
+                Id =1,
+                Nombre = "Villa Real",
+                Detalle = "Detalle de la villa",
+                ImageUrl = "",
+                Ocupantes = 5,
+                MetrosCuadrados = 50,
+                Tarifa = 50,
+                Amenidad = "",
+                FechaCreacion = DateTime.Now,
+                FechaActualizacion = DateTime.Now
+            },
+            new Villa()
+            {
+                Id = 2,
+                Nombre = "Premium Vista ala piscina",
+                Detalle = "Detalle de la villa",
+                ImageUrl = "",
+                Ocupantes = 5,
+                MetrosCuadrados = 50,
+                Amenidad = "",
+                FechaCreacion = DateTime.Now,
+                FechaActualizacion = DateTime.Now
+            }
+        );
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
---
+++ VillaApi/Migrations/20230621103316_databasetest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VillaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class databasetest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImageUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle de la villa", new DateTime(2023, 6, 21, 3, 33, 16, 362, DateTimeKind.Local).AddTicks(4569), new DateTime(2023, 6, 21, 3, 33, 16, 362, DateTimeKind.Local).AddTicks(4530), "", 50, "Villa Real", 5, 50.0 },
+                    { 2, "", "Detalle de la villa", new DateTime(2023, 6, 21, 3, 33, 16, 362, DateTimeKind.Local).AddTicks(4574), new DateTime(2023, 6, 21, 3, 33, 16, 362, DateTimeKind.Local).AddTicks(4573), "", 50, "Premium Vista ala piscina", 5, 0.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
