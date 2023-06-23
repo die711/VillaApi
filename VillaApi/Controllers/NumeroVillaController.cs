b2446c0f3626ed5ba5652c3e48ab62d78afa24d1
Diego Resendiz Ojeda <di_564@hotmail.com>
2023-06-22 19:37:49 -0700

    cambios numero villa

--- VillaApi/Controllers/NumeroVillaController.cs
+++ VillaApi/Controllers/NumeroVillaController.cs
@@ -1,6 +1,9 @@
+using System.Net;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using VillaApi.Models;
+using VillaApi.Models.Dto;
 using VillaApi.Repositorio.IRepositorio;
 
 namespace VillaApi.Controllers;
@@ -26,12 +29,18 @@ public class NumeroVillaController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<APIResponse>> GetNumeroVillas()
     {
         try
         {
             _logger.LogInformation("Obtener numeros de villas");
             IEnumerable<NumeroVilla> numeroVillas = await _numeroVillaRepo.ObtenerTodos(incluirPropiedades: "Villa");
+
+            _response.Resultado = _mapper.Map<IEnumerable<NumeroVillaDto>>(numeroVillas);
+            _response.EstatusCode = HttpStatusCode.OK;
+
+            return Ok(_response);
         }
         catch (Exception ex)
         {
@@ -42,4 +51,110 @@ public class NumeroVillaController : ControllerBase
         return _response;
     }
 
+    [HttpGet("Villa/{villaId:int}")]
+    public async Task<ActionResult<APIResponse>> GetNumeroVillasByVillaId(int villaId)
+    {
+        try
+        {
+            _logger.LogInformation("Obtener numeros de villas");
+
+            IEnumerable<NumeroVilla> numeroVillas =
+                await _numeroVillaRepo.ObtenerTodos(x => x.VillaId == villaId, incluirPropiedades: "Villa");
+
+            _response.Resultado = _mapper.Map<IEnumerable<NumeroVillaDto>>(numeroVillas);
+            _response.EstatusCode = HttpStatusCode.OK;
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.IsExitoso = false;
+            _response.ErrorMessages = new List<string>() { ex.ToString() };
+        }
+
+        return _response;
+    }
+
+    [HttpGet("id", Name = "GetNumeroVilla")]
+    public async Task<ActionResult<APIResponse>> GetNumeroVilla(int id)
+    {
+        try
+        {
+            if (id == 0)
+            {
+                _logger.LogError("Error al traer numero villa con Id "+id);
+                _response.EstatusCode = HttpStatusCode.BadRequest;
+                _response.IsExitoso = false;
+                return BadRequest(_response);
+            }
+
+            var numeroVilla = await _numeroVillaRepo.Obtener(x => x.VillaNo == id, incluirPropiedades:"Villa");
+
+            if (numeroVilla == null)
+            {
+                _response.EstatusCode = HttpStatusCode.NotFound;
+                _response.IsExitoso = false;
+                return NotFound();
+            }
+
+            _response.Resultado = _mapper.Map<NumeroVillaDto>(numeroVilla);
+            _response.EstatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+            
+        }
+        catch (Exception ex)
+        {
+            _response.IsExitoso = false;
+            _response.EstatusCode = HttpStatusCode.OK;
+        }
+
+        return _response;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<APIResponse>> CrearNumeroVilla([FromBody] NumeroVillaCreateDto? createDto)
+    {
+        try
+        {
+            if (createDto == null)
+                return BadRequest(createDto);
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (await _numeroVillaRepo.Obtener(v => v.VillaNo == createDto.VillaNo) != null)
+            {
+                ModelState.AddModelError("NumeroExiste","El numero de villa ya existe");
+                return BadRequest(ModelState);
+            }
+
+            if (await _villaRepo.Obtener(v => v.Id == createDto.VillaId) == null)
+            {
+                ModelState.AddModelError("ClaveForanea","El Id de la villa no existe");
+                return BadRequest(ModelState);
+            }
+
+            NumeroVilla modelo = _mapper.Map<NumeroVilla>(createDto);
+
+            modelo.FechaCreacion = DateTime.Now;
+            modelo.FechaActualizacion = DateTime.Now;
+
+            await _numeroVillaRepo.Crear(modelo);
+            _response.Resultado = modelo;
+            _response.EstatusCode = HttpStatusCode.Created;
+
+            return CreatedAtRoute("GetNumeroVilla", new { id = modelo.VillaNo }, _response);
+
+        }
+        catch (Exception ex)
+        {
+            _response.IsExitoso = false;
+            _response.ErrorMessages = new List<string>() { ex.ToString() };
+        }
+
+        return _response;
+    }
+    
 }
